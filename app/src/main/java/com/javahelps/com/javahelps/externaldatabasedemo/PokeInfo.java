86222e4f11c5c254fea201b1da69b7a500bbf144
Ricardo Chan <josericardochan@hotmail.com>
2018-12-23 08:00:34 -0600

    -Agregada la libreria de graficas junto con el demo de la graficaradar -En PokeInfo.java/'getview()','layout.otra' estaba comentado y en su lugar 'layout.activity_adaptador' estaba escrito, lo cual me causaba conflicto pues dicho xml no existe. Se comentó el segundo y se dejó activo el primero.

--- app/src/main/java/com/javahelps/com/javahelps/externaldatabasedemo/PokeInfo.java
+++ app/src/main/java/com/javahelps/com/javahelps/externaldatabasedemo/PokeInfo.java
@@ -2,7 +2,7 @@ package com.javahelps.com.javahelps.externaldatabasedemo;
 
 import android.content.Intent;
 import android.graphics.Color;
-import android.graphics.Paint;
+import android.graphics.Typeface;
 import android.graphics.drawable.Drawable;
 import android.graphics.drawable.GradientDrawable;
 import android.graphics.drawable.PaintDrawable;
@@ -14,6 +14,22 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.github.mikephil.charting.animation.Easing;
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.charts.RadarChart;
+import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.components.MarkerView;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.RadarData;
+import com.github.mikephil.charting.data.RadarDataSet;
+import com.github.mikephil.charting.data.RadarEntry;
+import com.github.mikephil.charting.formatter.IValueFormatter;
+import com.github.mikephil.charting.formatter.ValueFormatter;
+import com.github.mikephil.charting.interfaces.datasets.IDataSet;
+import com.github.mikephil.charting.interfaces.datasets.IRadarDataSet;
+import com.javahelps.com.javahelps.externaldatabasedemo.custom.RadarMarkerView;
+
 import java.util.ArrayList;
 
 public class PokeInfo extends AppCompatActivity {
@@ -22,6 +38,9 @@ public class PokeInfo extends AppCompatActivity {
     DatabaseAccess databaseAccessInfo;
     public String dato;
     public Informacion datosInfo;
+
+    private RadarChart chart;
+    protected Typeface tfLight;
 
     TextView NumeroInfo;
     TextView NombreInfo;
@@ -99,7 +118,77 @@ public class PokeInfo extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_poke_info);
 
+        chart = findViewById(R.id.chart);
+        chart.setBackgroundColor(Color.rgb(255, 255, 255));
+        tfLight = Typeface.createFromAsset(getAssets(), "OpenSans-Light.ttf");
+
+        chart.getDescription().setEnabled(false);
+
+        chart.setWebLineWidth(1f);
+        chart.setWebColor(Color.LTGRAY);
+        chart.setWebLineWidthInner(1f);
+        chart.setWebColorInner(Color.LTGRAY);
+        chart.setWebAlpha(100);
+
+        // create a custom MarkerView (extend MarkerView) and specify the layout
+        // to use for it
+        MarkerView mv = new RadarMarkerView(this, R.layout.radar_markerview);
+        mv.setChartView(chart); // For bounds control
+        chart.setMarker(mv); // Set the marker to the chart
+
+        setData();
+
+        chart.animateXY(1400, 1400, Easing.EaseInOutQuad);
+
+
+        XAxis xAxis = chart.getXAxis();
+        xAxis.setTypeface(tfLight);
+        xAxis.setTextSize(9f);
+        xAxis.setYOffset(0f);
+        xAxis.setXOffset(0f);
+        xAxis.setValueFormatter(new ValueFormatter() {
+
+            private final String[] mActivities = new String[]{"HP", "ATK", "DEF", "SPEED", "ATK.ESP", "DEF.ESP"};
+
+            @Override
+            public String getFormattedValue(float value) {
+                return mActivities[(int) value % mActivities.length];
+            }
+        });
+        xAxis.setTextColor(Color.RED);
+
+        YAxis yAxis = chart.getYAxis();
+        yAxis.setTypeface(tfLight);
+        yAxis.setLabelCount(5, false);
+        yAxis.setTextSize(9f);
+        yAxis.setAxisMinimum(0f);
+        yAxis.setAxisMaximum(80f);
+        yAxis.setDrawLabels(false);
+
+        Legend l = chart.getLegend();
+        l.setVerticalAlignment(Legend.LegendVerticalAlignment.TOP);
+        l.setHorizontalAlignment(Legend.LegendHorizontalAlignment.CENTER);
+        l.setOrientation(Legend.LegendOrientation.HORIZONTAL);
+        l.setDrawInside(false);
+        l.setTypeface(tfLight);
+        l.setXEntrySpace(7f);
+        l.setYEntrySpace(5f);
+        l.setTextColor(Color.RED);
+
+        for (IDataSet<?> set : chart.getData().getDataSets())
+            set.setDrawValues(!set.isDrawValuesEnabled());
+
+
+
+        if (chart.isRotationEnabled())
+            chart.setRotationEnabled(false);
+
+        chart.invalidate();
+
+
         parametros();
+
+
 
         Intent intent = getIntent(); //Obtengo los datos del intent
         Bundle extras = intent.getExtras(); //los datos obtenidos se guardan en extra
@@ -118,6 +207,57 @@ public class PokeInfo extends AppCompatActivity {
             Datos();
             Estilo();
         }
+    }
+
+    private void setData() {
+
+        float mul = 80;
+        float min = 20;
+        int cnt = 6;
+
+        ArrayList<RadarEntry> entries1 = new ArrayList<>();
+        ArrayList<RadarEntry> entries2 = new ArrayList<>();
+
+        // NOTE: The order of the entries when being added to the entries array determines their position around the center of
+        // the chart.
+        for (int i = 0; i < cnt; i++) {
+            float val1 = (float) (Math.random() * mul) + min;
+            entries1.add(new RadarEntry(val1));
+
+            float val2 = (float) (Math.random() * mul) + min;
+            entries2.add(new RadarEntry(val2));
+        }
+
+        RadarDataSet set1 = new RadarDataSet(entries1, "Last Week");
+        set1.setColor(Color.rgb(0, 204, 0));
+        set1.setFillColor(Color.rgb(51, 153, 255));
+        set1.setDrawFilled(true);
+        set1.setFillAlpha(180);
+        set1.setLineWidth(2f);
+        set1.setDrawHighlightCircleEnabled(true);
+        set1.setDrawHighlightIndicators(false);
+
+        RadarDataSet set2 = new RadarDataSet(entries2, "This Week");
+        set2.setColor(Color.rgb(0, 204, 0));
+        set2.setFillColor(Color.rgb(51, 153, 255));
+        set2.setDrawFilled(true);
+        set2.setFillAlpha(180);
+        set2.setLineWidth(2f);
+        set2.setDrawHighlightCircleEnabled(true);
+        set2.setDrawHighlightIndicators(false);
+
+        ArrayList<IRadarDataSet> sets = new ArrayList<>();
+        sets.add(set1);
+        sets.add(set2);
+
+        RadarData data = new RadarData(sets);
+        //data.setValueTypeface(tfLight);
+        data.setValueTextSize(8f);
+        data.setDrawValues(false);
+        data.setValueTextColor(Color.WHITE);
+
+        chart.setData(data);
+        chart.invalidate();
     }
 
     public void parametros()
